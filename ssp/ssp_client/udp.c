3d55eabd8299fec2571c1a3e80dc5046be93e37d
Norton Allen <allen@huarp.harvard.edu>
2006-12-08 14:21:40 +0000

    Tested on one system

--- ssp/ssp_client/udp.c
+++ ssp/ssp_client/udp.c
@@ -5,6 +5,7 @@ static int udp_socket;
 int udp_create(void) {
   struct sockaddr_in servAddr;
   int port, rc;
+  socklen_t servAddr_len = sizeof(servAddr);
 
   /* socket creation */
   udp_socket=socket(AF_INET, SOCK_DGRAM, 0);
@@ -22,10 +23,15 @@ int udp_create(void) {
     printf("cannot bind port number %d \n",  0);
     exit(1);
   }
+  rc = getsockname( udp_socket, (struct sockaddr *)&servAddr,
+    &servAddr_len);
   port = ntohs(servAddr.sin_port);
+  printf("UDP port is %d\n", port );
+  fflush(stdout);
   return port;
 }
 
+static int max_msg_size = 0;
 int udp_receive(void) {
     struct sockaddr_in cliAddr;
 	char msg[UDP_MAX_MSG];
@@ -34,6 +40,9 @@ int udp_receive(void) {
     cliLen = sizeof(cliAddr);
     n = recvfrom(udp_socket, msg, UDP_MAX_MSG, 0, 
 		 (struct sockaddr *) &cliAddr, &cliLen);
-
+    if ( n > max_msg_size ) {
+    	max_msg_size = n;
+    	printf("Maximum UDP message received: %d\n", n );
+    }
     return n;
 }
