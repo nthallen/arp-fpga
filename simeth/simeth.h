3927c4129db3641af1172f60e58acb22e6518ade
Norton Allen <allen@huarp.harvard.edu>
2007-01-24 18:41:29 +0000

    Split out interrupt definitions to avoid include file conflicts

--- simeth/simeth.h
+++ simeth/simeth.h
@@ -14,22 +14,13 @@ extern void *serverAppThread(void *arg);
 extern void *tcpThread( void *socketptr );
 extern int ethernet_init(void);
 extern void check_fifo_status( int status, char *where );
-extern void xfr_disable(void);
-extern void xfr_enable(void);
-extern void xfr_init(void);
-extern void sg_handler(void *);
 
 #define SCAN_TZ 100
 #define SCAN_TRAMP 1000
 #define SCAN_LENGTH (7+SCAN_TRAMP+2*SCAN_TZ)
-#define MAX_SCAN_LENGTH 2048
-#define SCAN_GUARD 100
 #define SSP_SERVER_PORT 1500
 #define SSP_MAX_MSG 80
 #define EMAC_INTERRUPT_ID XPAR_OPB_INTC_0_ETHERNET_MAC_IP2INTC_IRPT_INTR
-#define SG_INTR_ID XPAR_OPB_INTC_0_SCAN_GEN_SM_0_SCANINTR_INTR
-
-extern unsigned int scan[];
 
 /* External Declarations */
 extern XEmacIf_Config XEmacIf_ConfigTable[];
