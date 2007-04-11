c1189913a25384129f9a1f0cc063cdb2af50d3c9
Norton Allen <allen@huarp.harvard.edu>
2007-04-11 15:19:00 +0000

    Test configuration

--- ssp/ssp_client/udp_demo.c
+++ ssp/ssp_client/udp_demo.c
@@ -28,14 +28,71 @@
 #define SCANS_PER_TEST 100
 int verbosity = 0;
 
+void process_scan_set( int scan_length, int n_scans, FILE *logfp, char *dumpfile ) {
+	char cmdbuf[80];
+	int scan_size = (scan_length+1)*sizeof(long);
+	int n;
+  uint64_t start_time, end_time;
+	double duration;
+	double sdS, sdS2, mean_dS, std_dS;
+	long int last_scan = 0;
+	int total_bytes, i, j;
+	unsigned long int scan[4096];
+	uint64_t cps = SYSPAGE_ENTRY(qtime)->cycles_per_sec;
+	FILE *dumpfp = NULL;
+	
+	if (dumpfile != NULL ) {
+		dumpfp = fopen( dumpfile, "w" );
+	}
+	sprintf(cmdbuf, "NS:%d\r\n", scan_length );
+	tcp_send(cmdbuf);
+	tcp_send("EN\r\n");
+	for (;;) {
+		n = udp_receive(scan);
+		if ( n == scan_size ) break;
+		fprintf(stderr, "Expected %d bytes, received %d\n", scan_size, n );
+	}
+	sdS = sdS2 = 0.;
+	start_time = ClockCycles( );
+	// read a bunch of scans
+	for (i = 0; i < n_scans; i++ ) {
+		n = udp_receive(scan );
+		if ( n != scan_size )
+		  fprintf( stderr, "!Expected %d bytes, received %d\n", scan_size, n);
+		else if ( i == 0 ) {
+			printf("  First scan = %08lX\n", scan[scan_length] );
+		} else {
+			unsigned long int dS = scan[scan_length] - last_scan;
+			sdS += dS;
+			sdS2 += dS*dS;
+		}
+		last_scan = scan[scan_length];
+		if ( dumpfp ) {
+			fprintf( dumpfp, "Scan %d\n", i );
+			for ( j = 0; j <= scan_length; j++ )
+			  fprintf( dumpfp, "%08lX\n", scan[j] );
+		}
+	}
+	end_time = ClockCycles( );
+	// send DA command
+	tcp_send("DA\r\n");
+	// Calculate throughput
+	duration = (end_time - start_time) / (double)cps;
+	total_bytes = scan_size * n_scans;
+	mean_dS = sdS/n_scans;
+	std_dS = sqrt(sdS2/n_scans - mean_dS*mean_dS);
+	printf( "%4d  %4d  %5.4f %.1f %.2f\n",
+	  scan_length,n_scans, duration, mean_dS, std_dS );
+	fprintf(logfp, "%4d  %4d  %5.4f %.1f %.2f\n",
+	  scan_length,n_scans, duration, mean_dS, std_dS );
+}
+
 int main( int argc, char **argv ) {
 	char cmdbuf[80];
-	int i, scan_length;
 	int udp_port = udp_create();
-	unsigned long int scan[4096];
 	FILE *ofp;
+	int scan_length;
 
-	uint64_t cps = SYSPAGE_ENTRY(qtime)->cycles_per_sec;
 	printf("Opened UDP port %d\n", udp_port);
 	// open TCP connection to SSP board
 	tcp_create("10.0.0.200");
@@ -47,52 +104,13 @@ int main( int argc, char **argv ) {
 	  fprintf(stderr,"Cannot open udp_demo.log\n");
 	  exit(1);
 	}
-	for ( scan_length = 25; scan_length < 1207; scan_length += 25 ) {
-		int scan_size = (scan_length+1)*sizeof(long);
-		int n;
-    uint64_t start_time, end_time;
-		double duration;
-		double sdS, sdS2, mean_dS, std_dS;
-		long int last_scan;
-		int total_bytes;
-		
-		sprintf(cmdbuf, "NS:%d\r\n", scan_length );
-		tcp_send(cmdbuf);
-  	tcp_send("EN\r\n");
-		for (;;) {
-			n = udp_receive(scan);
-			if ( n == scan_size ) break;
-			fprintf(stderr, "Expected %d bytes, received %d\n", scan_size, n );
-		}
-		sdS = sdS2 = 0.;
-		start_time = ClockCycles( );
-		// read a bunch of scans
-		for (i = 0; i < SCANS_PER_TEST; i++ ) {
-			n = udp_receive(scan );
-			if ( n != scan_size )
-			  fprintf( stderr, "!Expected %d bytes, received %d\n", scan_size, n);
-			else if ( i == 0 ) {
-				printf("  First scan = %08lX\n", scan[scan_length] );
-			} else {
-				unsigned long int dS = scan[scan_length] - last_scan;
-				sdS += dS;
-				sdS2 += dS*dS;
-			}
-			last_scan = scan[scan_length];
-		}
-		end_time = ClockCycles( );
-		// send DA command
-		tcp_send("DA\r\n");
-		// Calculate throughput
-		duration = (end_time - start_time) / (double)cps;
-		total_bytes = scan_size * SCANS_PER_TEST;
-		mean_dS = sdS/SCANS_PER_TEST;
-		std_dS = sqrt(sdS2/SCANS_PER_TEST - mean_dS*mean_dS);
-		printf( "%4d  %4d  %5.4f %.1f %.2f\n",
-		  scan_length,SCANS_PER_TEST, duration, mean_dS, std_dS );
-		fprintf(ofp, "%4d  %4d  %5.4f %.1f %.2f\n",
-		  scan_length,SCANS_PER_TEST, duration, mean_dS, std_dS );
+//	for ( scan_length = 999; scan_length < 1207; scan_length += 25 ) {
+	for ( scan_length = 999; scan_length > 25; scan_length -= 25 ) {
+		process_scan_set( scan_length, SCANS_PER_TEST, ofp, NULL );
 	}
+//  for (; ;) {
+//    process_scan_set( 999, 100, ofp, NULL );
+//  }
 	tcp_send("EX\r\n");
 	fclose(ofp);
 	return 0;
