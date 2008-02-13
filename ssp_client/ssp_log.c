e8808e61c8db2a3aef5fe521ec9a1370f7c017ed
Norton Allen <allen@huarp.harvard.edu>
2008-02-13 15:16:14 +0000

    Support more options: NAvg. Log scan serial number and report in ssp_data

--- ssp_client/ssp_log.c
+++ ssp_client/ssp_log.c
@@ -10,6 +10,7 @@
   Commands:
     NS:xxxx N_Samples
     NC:xxxx N_Coadd
+    NA:xxxx N_Average
     UP:xxxxx UDP Port Number
     EN Enable
     DA Disable
@@ -20,19 +21,22 @@
 */
 #include "ssp_get.h"
 #include <stdlib.h>
+#include <unistd.h>
 #include <time.h>
 #include <math.h>
+#include <setjmp.h>
 #include "nortlib.h"
 #include "mlf.h"
 
 int verbosity = 1;
 
+/* If index == 0, this is a cleanup operation */
 void move_lock( unsigned long index ) {
   static unsigned long prev_index;
   static int initialized = 0;
   char oldname[80], newname[80];
   
-  sprintf( newname, "LOG/ssp_%lu.lock", index );
+  sprintf( newname, "LOG/ssp_%lu.lock", index == 0L ? prev_index+1 : index );
   if ( ! initialized ) {
     FILE *fp = fopen( newname, "w" );
     fprintf( fp, "lock file\n" );
@@ -40,17 +44,35 @@ void move_lock( unsigned long index ) {
     initialized = 1;
   } else {
     sprintf( oldname, "LOG/ssp_%lu.lock", prev_index );
-    rename( oldname, newname );
+    if ( index == 0 ) {
+      unlink(oldname);
+      unlink(newname);
+      initialized = 0;
+    } else {
+      rename( oldname, newname );
+    }
   }
   prev_index = index;
+}
+
+void cleanup(void) {
+  unlink(PID_FILE);
+  move_lock(0L);
+  nl_error( 0, "cleanup" );
+}
+
+static sigjmp_buf env;
+
+void sigint_handler(int sig) {
+  siglongjmp(env,1);
 }
 
 int main( int argc, char **argv ) {
   char cmdbuf[80];
   long int scan[4096];
   int udp_port;
-  int scan_length = 999;
-  int scan_size = (scan_length+1)*sizeof(long);
+  int scan_length = 0;
+  int scan_size;
   char *ssp_hostname;
 
   time_t last_rpt = 0, now;
@@ -58,44 +80,68 @@ int main( int argc, char **argv ) {
   ssp_hostname = getenv("SSP_HOSTNAME");
   if ( ssp_hostname == 0 ) ssp_hostname = "10.0.0.200";
   else nl_error(0, "Addressing SSP %s", ssp_hostname );
+  { FILE *fp = fopen( PID_FILE, "w" );
+    if ( fp == 0 ) nl_error( 3, "Unable to write " PID_FILE );
+    fprintf( fp, "%d\n", getpid() );
+    fclose(fp);
+  }
   move_lock(1L);
-  udp_port = udp_create();
-  nl_error(0, "Opened UDP port %d", udp_port);
-  // open TCP connection to SSP board
-  tcp_create(ssp_hostname);
-  // transmit the command including UDP socket number
-  sprintf(cmdbuf, "UP:%d\r\n", udp_port );
-  tcp_send(cmdbuf);
-  sprintf(cmdbuf, "NS:%d\r\n", scan_length );
-  nl_error( 0, "Setting scan_length to %d\n", scan_length );
-  tcp_send(cmdbuf);
-  while (tcp_send("EN\r\n") == 503 ) sleep(1);
-  tcp_close();
-  for (;;) {
-    int n = udp_receive(scan);
-    if ( n != scan_size ) {
-      nl_error( mlf->index ? 2 : 1,
-        "Expected %d bytes, received %d\n", scan_size, n );
-    } else {
-      int j;
-      FILE *ofp = mlf_next_file(mlf);
-      for ( j = 0; j < scan_length; j++ ) {
-	// temporary fix for signed data
-	// signed long cts = scan[j];
-	// if ( cts > 32767 )
-	//   cts -= 65536L;
-	fprintf( ofp, "%10ld\n", scan[j] );
+  if ( sigsetjmp( env, 1 ) == 0 ) {
+    int i;
+    unsigned long index;
+    
+    atexit(cleanup);
+    signal(SIGINT, sigint_handler);
+    udp_port = udp_create();
+    nl_error(0, "Opened UDP port %d", udp_port);
+    // open TCP connection to SSP board
+    tcp_create(ssp_hostname);
+    // transmit the command including UDP socket number
+    sprintf(cmdbuf, "UP:%d\r\n", udp_port );
+    tcp_send(cmdbuf);
+    for ( i = 1; i < argc; i++ ) {
+      sprintf( cmdbuf, "%s\r\n", argv[i] );
+      if ( strncmp( cmdbuf, "NS:", 3 ) == 0 )
+        scan_length = atoi(cmdbuf+3);
+      else if ( strncmp( cmdbuf, "IX:", 3 ) == 0 ) {
+      	index = strtoul(cmdbuf+3, NULL, 10);
+      	mlf_set_index(mlf, index);
+      	move_lock(index);
+        continue; // skip the send
       }
-      fclose(ofp);
-      move_lock(mlf->index+1);
-      now = time(NULL);
-      if ( difftime(now, last_rpt) > 5 ) {
-	nl_error( 0, "Recorded Scan %lu", mlf->index );
-	last_rpt = now;
+      tcp_send(cmdbuf);
+    }
+    if ( scan_length == 0 ) {
+      scan_length = 1024;
+      sprintf(cmdbuf, "NS:%d\r\n", scan_length );
+      nl_error( 0, "Setting scan_length to %d\n", scan_length );
+      tcp_send(cmdbuf);
+    }
+    scan_size = (scan_length+1)*sizeof(long);
+    while (tcp_send("EN\r\n") == 503 ) sleep(1);
+    tcp_close();
+    for (;;) {
+      int n = udp_receive(scan);
+      if ( n != scan_size ) {
+        nl_error( mlf->index ? 2 : 1,
+          "Expected %d bytes, received %d\n", scan_size, n );
+      } else {
+      	int j;
+      	FILE *ofp = mlf_next_file(mlf);
+      	for ( j = 0; j <= scan_length; j++ ) {
+      	  fprintf( ofp, "%10ld\n", scan[j] );
+      	}
+      	fclose(ofp);
+      	move_lock(mlf->index+1);
+      	now = time(NULL);
+      	if ( difftime(now, last_rpt) > 5 ) {
+      	  nl_error( 0, "Recorded Scan %lu", mlf->index );
+      	  last_rpt = now;
+      	}
       }
     }
   }
-
+  nl_error( 0, "Normal exit" );
   // tcp_send("EX\r\n");
   return 0;
 }
