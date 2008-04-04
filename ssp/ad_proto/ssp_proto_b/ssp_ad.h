386622c32afaa7a98cd0e0cdd73652bf845216ae
Norton Allen <allen@huarp.harvard.edu>
2008-04-04 13:10:01 +0000

    Cleanly compiling with changes for coadd engine

--- ssp/ad_proto/ssp_proto_b/ssp_ad.h
+++ ssp/ad_proto/ssp_proto_b/ssp_ad.h
@@ -13,9 +13,36 @@ extern void *tcpThread( void *socketptr );
 extern int ethernet_init(void);
 extern int check_fifo_status( int status, char *where );
 
-#define SCAN_TZ 100
-#define SCAN_TRAMP 1000
-#define SCAN_LENGTH (7+SCAN_TRAMP+2*SCAN_TZ)
+typedef struct {
+	unsigned short CC;
+	unsigned short RV;
+	unsigned short EN; /* bool */
+	unsigned short NS, NA, NC;
+	unsigned short NP; /* udp port number */
+	unsigned short NE;
+	signed short TL;
+	unsigned short TrigConfig;
+} ssp_config_t;
+extern ssp_config_t ssp_config;
+
+
+#define SSP_MAX_SAMPLES 4096
+#define SSP_MAX_PREADD 5
+#define SSP_MAX_COADD 7
+#define SSP_MAX_CHANNELS 3
+#define SSP_MAX_NE ((1<<SSP_MAX_CHANNELS)-1)
+#define SSP_NE_LSB 5
+#define SSP_NE_MASK (SSP_MAX_NE<<SSP_NE_LSB)
+#define SSP_TRIG_RISING 0x8
+#define SSP_TRIG_AE 0x10
+#define SSP_TRIG_SRC_MASK 0x6
+#define SSP_TRIG_SRC_LSB 1
+#define SSP_TRIG_SRC_MAX 3
+#define SSP_TRIG_MASK (SSP_TRIG_SRC_MASK|SSP_TRIG_AE|SSP_TRIG_RISING)
+#define SSP_RESET_MASK 1
+#define SSP_CONTROL_MASK 0x3F
+
+#define SSP_MAX_SCAN_LENGTH (7+SSP_MAX_CHANNELS*SSP_MAX_SAMPLES)
 #define SSP_SERVER_PORT 1500
 #define SSP_MAX_MSG 80
 #define EMAC_INTERRUPT_ID XPAR_XPS_INTC_0_ETHERNET_MAC_IP2INTC_IRPT_INTR
