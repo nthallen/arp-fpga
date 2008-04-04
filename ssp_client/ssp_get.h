308c7d34ef4297154694722deb1357600d04c4fe
Norton Allen <allen@huarp.harvard.edu>
2008-04-04 13:40:09 +0000

    Changes to support coadd engine and multiple channels.

--- ssp_client/ssp_get.h
+++ ssp_client/ssp_get.h
@@ -1,5 +1,6 @@
 #ifndef ssp_get_h_included
 #define ssp_get_h_included
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,15 +11,16 @@
 #include <string.h> /* memset() */
 #include <errno.h>
 #include <ctype.h>
+#include "ssp_ad.h"
 
 extern int udp_create(void);
 extern int udp_receive(unsigned long int *scan );
 extern int tcp_create(char *hostname);
 extern int tcp_send( char * cmd );
 extern int tcp_close(void);
-#define SSP_SERVER_PORT 1500
-#define RECV_BUF_SIZE 80
-#define UDP_MAX_MSG (4096*4)
+
+#define RECV_BUF_SIZE SSP_MAX_CTRL_MSG
+
 extern int verbosity;
 #define PID_FILE "LOG/ssp_log.pid"
 
