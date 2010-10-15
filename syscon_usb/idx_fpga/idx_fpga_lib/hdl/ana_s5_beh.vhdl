c9c158e64ec87151620dffdbed1c1e41e9781bf3
Norton Allen <allen@huarp.harvard.edu>
2010-10-15 19:43:17 +0000

    ana_s16 and test bench

--- syscon_usb/idx_fpga/idx_fpga_lib/hdl/ana_s5_beh.vhdl
+++ syscon_usb/idx_fpga/idx_fpga_lib/hdl/ana_s5_beh.vhdl
@@ -13,6 +13,8 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_arith.all;
+LIBRARY idx_fpga_lib;
+USE idx_fpga_lib.All;
 
 ENTITY ana_s5 IS
   GENERIC (
@@ -34,9 +36,21 @@ END ENTITY ana_s5;
 
 --
 ARCHITECTURE beh OF ana_s5 IS
-  SIGNAL n_bits : unsigned(3 DOWNTO 0);
   SIGNAL RDI : std_ulogic_vector(4 DOWNTO 0);
-  SIGNAL SR : std_ulogic_vector(4 DOWNTO 0);
+  COMPONENT ana_s5s
+     PORT (
+        DI    : IN     std_ulogic_vector(4 DOWNTO 0);
+        SDI   : IN     std_ulogic;
+        Start : IN     std_ulogic;
+        clk   : IN     std_logic;
+        rst   : IN     std_logic;
+        DO    : OUT    std_ulogic_vector(4 DOWNTO 0);
+        RDY   : OUT    std_ulogic;
+        SCK   : OUT    std_ulogic;
+        SDO   : OUT    std_ulogic
+     );
+  END COMPONENT;
+  FOR ALL : ana_s5s USE ENTITY idx_fpga_lib.ana_s5s;
 BEGIN
   SRDI : Process (CLK) IS
   Begin
@@ -49,5 +63,18 @@ BEGIN
       end if;
     end if;
   End Process;
+   --  hds hds_inst
+   ana_s5s_i : ana_s5s
+      PORT MAP (
+         DI    => DI,
+         SDI   => SDI,
+         Start => Start,
+         clk   => CLK,
+         rst   => RST,
+         DO    => DO,
+         RDY   => RDY,
+         SCK   => SCK,
+         SDO   => SDO
+      );
 END ARCHITECTURE beh;
 
