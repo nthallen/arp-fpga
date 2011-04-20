e340ba5b685f4130c8587be7ac09c27c6270bde0
Norton Allen <allen@huarp.harvard.edu>
2011-04-20 17:01:35 +0000

    Still Build 14: Adding Voltage Monitor to HTW

---
+++ syscon_usb/DACS_V1_0_ISE/vhdl/idx_fpga_lib/vm_addr_beh.vhdl
@@ -0,0 +1,59 @@
+--
+-- VHDL Architecture idx_fpga_lib.vm_addr.beh
+--
+-- Created:
+--          by - nort.UNKNOWN (NORT-NBX200T)
+--          at - 16:49:48 04/19/2011
+--
+-- using Mentor Graphics HDL Designer(TM) 2009.2 (Build 10)
+--
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_arith.all;
+
+ENTITY vm_addr IS
+   GENERIC( 
+      BASE_ADDR : unsigned(15 DOWNTO 0) := X"0360"
+   );
+   PORT( 
+      Addr  : IN     std_logic_vector (15 DOWNTO 0);
+      BdEn  : OUT    std_ulogic;
+      RegEn : OUT    std_ulogic_vector (4 DOWNTO 0)
+   );
+
+-- Declarations
+
+END vm_addr ;
+
+--
+ARCHITECTURE beh OF vm_addr IS
+  SIGNAL RegEn_int : std_ulogic_vector(4 DOWNTO 0);
+BEGIN
+  decode : Process (Addr, RegEn_int ) Is
+  Begin
+    BdEn <= '0';
+    RegEn_int <= (others => '0');
+    if Addr = BASE_ADDR+0 then
+      RegEn_int(0) <= '1';
+    elsif Addr = BASE_ADDR+2 then
+      RegEn_int(1) <= '1';
+    elsif Addr = BASE_ADDR+4 then
+      RegEn_int(2) <= '1';
+    elsif Addr = BASE_ADDR+6 then
+      RegEn_int(3) <= '1';
+    elsif Addr = BASE_ADDR+8 then
+      RegEn_int(4) <= '1';
+    end if;
+
+    for i in 0 to 4 loop
+      if RegEn_int(i) = '1' then
+        BdEn <= '1';
+      end if;
+    end loop;
+    
+  End Process;
+
+  RegEn <= RegEn_int;
+  
+END ARCHITECTURE beh;
+
---
+++ syscon_usb/DACS_V1_0_ISE/vhdl/idx_fpga_lib/vm_dprams_beh.vhdl
@@ -0,0 +1,71 @@
+--
+-- VHDL Architecture idx_fpga_lib.vm_dprams.beh
+--
+-- Created:
+--          by - nort.UNKNOWN (NORT-NBX200T)
+--          at - 16:56:04 04/19/2011
+--
+-- using Mentor Graphics HDL Designer(TM) 2009.2 (Build 10)
+--
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_arith.all;
+LIBRARY idx_fpga_lib;
+
+ENTITY vm_dprams IS
+   PORT( 
+      F8M   : IN     std_ulogic;
+      RdEn  : IN     std_ulogic;
+      RegEn : IN     std_ulogic_vector (4 DOWNTO 0);
+      WrEn  : IN     std_ulogic_vector (4 DOWNTO 0);
+      wData : IN     std_logic_vector (15 DOWNTO 0);
+      rData : OUT    std_logic_vector (15 DOWNTO 0)
+   );
+
+-- Declarations
+
+END vm_dprams ;
+
+--
+ARCHITECTURE beh OF vm_dprams IS
+  COMPONENT ptrh_dpram
+     PORT (
+        F8M   : IN     std_ulogic;
+        RdEn  : IN     std_ulogic;
+        hold  : IN     std_logic;
+        rData : OUT    std_logic_vector(15 DOWNTO 0);
+        wData : IN     std_logic_vector(15 DOWNTO 0);
+        WrEn  : IN     std_ulogic;
+        Full  : OUT    std_ulogic
+     );
+  END COMPONENT;
+  FOR ALL : ptrh_dpram USE ENTITY idx_fpga_lib.ptrh_dpram;
+  SIGNAL Full : std_ulogic_vector(4 DOWNTO 0);
+  type iRData_t is array (4 DOWNTO 0) of std_logic_vector(15 DOWNTO 0);
+  SIGNAL iRData : iRData_t;
+BEGIN
+  rams : for i in 0 to 4 generate
+    lo: ptrh_dpram
+      PORT MAP (
+         F8M   => F8M,
+         RdEn  => RdEn,
+         hold  => '0',
+         rData => iRData(i),
+         wData => wData,
+         WrEn  => WrEn(i),
+         Full  => Full(i)
+      );
+  end generate;
+  
+  rData_p : Process (RegEn, iRData) Is
+  Begin
+    rData <= (others => '0');
+    for i in 4 DOWNTO 0 loop
+      if RegEn(i) = '1' then
+        rData <= iRData(i);
+      end if;
+    end loop;
+  End Process;
+
+END ARCHITECTURE beh;
+
