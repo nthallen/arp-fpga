e810fccec39eed5cc49aa185a7c0addef85de707
Norton Allen <allen@huarp.harvard.edu>
2011-11-01 14:08:42 +0000

    i2c_half_switch written

--- syscon_usb/idx_fpga/idx_fpga_lib/hdl/i2c_half_switch_syn.vhdl
+++ syscon_usb/idx_fpga/idx_fpga_lib/hdl/i2c_half_switch_syn.vhdl
@@ -16,13 +16,16 @@ USE idx_fpga_lib.ptrhm.all;
 USE idx_fpga_lib.All;
 
 ENTITY i2c_half_switch IS
-   PORT( 
-      ISwitch      : IN     std_ulogic_vector (N_ISBITS-1 DOWNTO 0);
-      scl_pad_o    : IN     std_logic;
-      scl_padoen_o : IN     std_logic;
-      scl_pad_i    : OUT    std_logic;
-      scl          : INOUT  std_logic_vector (N_ISBITS-1 DOWNTO 0)
-   );
+  GENERIC (
+    N_ISBITS : integer range 20 downto 2 := 4
+  );
+  PORT( 
+    En       : IN     std_ulogic_vector (N_ISBITS-1 DOWNTO 0);
+    pad_o    : IN     std_logic;
+    padoen_o : IN     std_logic;
+    pad_i    : OUT    std_logic;
+    pad      : INOUT  std_logic_vector (N_ISBITS-1 DOWNTO 0)
+  );
 
 -- Declarations
 
@@ -31,5 +34,30 @@ END i2c_half_switch ;
 --
 ARCHITECTURE syn OF i2c_half_switch IS
 BEGIN
+  ipad : Process (pad_o, padoen_o, En, pad) Is
+  Begin
+    pad_i <= '1';
+    if padoen_o = '0' AND pad_o = '0' then
+      pad_i <= '0';
+    else
+      for i in N_ISBITS-1 DOWNTO 0 loop
+        if pad(i) = '0' then
+          pad_i <= '0';
+        end if;
+      end loop;
+    end if;
+  End Process;
+  
+  opad : Process (pad_o, padoen_o, En) Is
+  Begin
+    for i in N_ISBITS-1 DOWNTO 0 loop
+      if En(i) = '1' AND padoen_o = '0' AND pad_o = '0' then
+        pad(i) <= '0';
+      else
+        pad(i) <= 'Z';
+      end if;
+    end loop;
+  End Process;
+  
 END ARCHITECTURE syn;
 
