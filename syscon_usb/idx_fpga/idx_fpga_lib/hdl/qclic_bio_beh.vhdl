5dba20f7a50c543839e43508c2c37dc597821634
Norton Allen <allen@huarp.harvard.edu>
2011-12-13 20:39:46 +0000

    QCLI Controller Start

---
+++ syscon_usb/idx_fpga/idx_fpga_lib/hdl/qclic_bio_beh.vhdl
@@ -0,0 +1,40 @@
+--
+-- VHDL Architecture idx_fpga_lib.qclic_bio.beh
+--
+-- Created:
+--          by - nort.UNKNOWN (NORT-NBX200T)
+--          at - 14:42:08 12/13/2011
+--
+-- using Mentor Graphics HDL Designer(TM) 2010.3 (Build 21)
+--
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_unsigned.all;
+USE ieee.std_logic_arith.all;
+
+ENTITY qclic_bio IS
+   PORT( 
+      o : IN     std_ulogic;
+      io   : INOUT  std_logic;
+      i : OUT    std_ulogic
+   );
+
+-- Declarations
+
+END qclic_bio ;
+
+--
+ARCHITECTURE beh OF qclic_bio IS
+BEGIN
+  i <= io;
+  
+  output : Process (o) Is
+  Begin
+    if o = '1' then
+      io <= '1';
+    else
+      io <= 'Z';
+    end if;
+  End Process;
+END ARCHITECTURE beh;
+
