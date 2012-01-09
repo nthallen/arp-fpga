8aace735cef77969269503947cf8c74b7ba1ad22
Norton Allen <allen@huarp.harvard.edu>
2012-01-09 21:42:36 +0000

    PDACS_Carbon ready for Synthesis

--- syscon_usb/idx_fpga/idx_fpga_lib/hdl/PDACS_Carbon_beh.vhdl
+++ syscon_usb/idx_fpga/idx_fpga_lib/hdl/PDACS_Carbon_beh.vhdl
@@ -20,21 +20,23 @@ ENTITY PDACS_Carbon IS
   GENERIC (
     DACS_BUILD_NUMBER : std_logic_vector(15 DOWNTO 0) := X"0010";
     INSTRUMENT_ID : std_logic_vector(15 DOWNTO 0) := X"0003";
-    CTR_UG_N_BDS : integer range 5 downto 0 := 3;
-    
-    N_PTRH : integer range 16 downto 1 := 3;
-    N_ISBITS    : integer range 8 downto 1 := 2;
-    ESID        : ESID_array := ( 0, 1, 1 );
-    ESwitchBit  : ESB_array  := ( 0, 0, 1 );
-    ISwitchBit  : ISB_array  := ( 0, 1 );
-    ESwitchAddr : ESA_array  := ( "0000000", "1110000" );
+    CTR_UG_N_BDS : integer range 5 downto 0 := 0;
+    N_QCLICTRL : integer range 5 downto 0 := 3;    
 
-    N_AO_CHIPS : natural range 15 downto 1 := 2;
+    N_PTRH : integer range 16 downto 1 := 9;
+    N_ISBITS    : integer range 8 downto 1 := 5;
+    ESID        : ESID_array := ( 0, 1, 1, 1, 1, 1, 2, 3, 4 );
+    ESwitchBit  : ESB_array  := ( 0, 0, 1, 2, 4, 5, 0, 0, 0 );
+    ISwitchBit  : ISB_array  := ( 0, 1, 2, 3, 4 );
+    ESwitchAddr : ESA_array  := ( "0000000", "1110000", "0000000", "0000000", "0000000" );
+
+    N_AO_CHIPS : natural range 15 downto 1 := 4;
     IDX_N_CHANNELS : integer range 15 downto 1 := 1;
-    DIGIO_N_CONNECTORS : integer range 4 DOWNTO 1 := 4;
-    DIGIO_FORCE_DIR : std_ulogic_vector := "111111111111000111110111";
-    DIGIO_FORCE_DIR_VAL : std_ulogic_vector := "000000001111000111100110";
-    CMD_PROC_N_CMDS : integer := 32
+    DIGIO_N_CONNECTORS : integer range 4 DOWNTO 1 := 5;
+    -- FORCE_DIR vectors are indexed 0 to 29
+    DIGIO_FORCE_DIR : std_ulogic_vector :=     "111111111111111111111111100000";
+    DIGIO_FORCE_DIR_VAL : std_ulogic_vector := "000000000000011111111111100000";
+    CMD_PROC_N_CMDS : integer := 50
   );
   PORT (
     AI_AD_CNV : OUT std_ulogic_vector ( 1 DOWNTO 0 );
@@ -46,7 +48,7 @@ ENTITY PDACS_Carbon IS
     AI_MUX0_A : OUT std_ulogic_vector ( 2 DOWNTO 0 );
     AI_MUX1_A : OUT std_ulogic_vector ( 2 DOWNTO 0 );
 
-    DA_CS_B : OUT std_logic_vector ( 1 DOWNTO 0 );
+    DA_CS_B : OUT std_logic_vector(1 DOWNTO 0);
     DA_CLR_B : OUT std_ulogic;
     DA_LDAC_B : OUT std_ulogic;
     DA_SCK : OUT std_ulogic;
@@ -148,7 +150,7 @@ ARCHITECTURE beh OF PDACS_Carbon IS
    SIGNAL idx_LimO                       : std_ulogic_vector(IDX_N_CHANNELS-1 DOWNTO 0);
    SIGNAL idx_ZR                         : std_ulogic_vector(IDX_N_CHANNELS-1 DOWNTO 0);
    SIGNAL dig_Dir                        : std_logic_vector( DIGIO_N_CONNECTORS*6-1 DOWNTO 0);
-   SIGNAL dig_io_nc                      : std_logic_vector( 34 DOWNTO 0);
+   SIGNAL dig_io_nc                      : std_logic_vector( 46 DOWNTO 0);
    SIGNAL ana_in_CS5                     : std_ulogic;
    SIGNAL ana_in_Conv                    : std_ulogic;
    SIGNAL ana_in_Row                     : std_ulogic_vector(5 DOWNTO 0);
@@ -157,7 +159,12 @@ ARCHITECTURE beh OF PDACS_Carbon IS
    SIGNAL RST                            : std_ulogic;
    SIGNAL Collision                      : std_ulogic;
    SIGNAL DACS_switches                  : std_logic_vector(7 DOWNTO 0);
-   
+   SIGNAL DA_SCK_int                     : std_ulogic;
+   SIGNAL DA_SDI_int                     : std_ulogic;
+   SIGNAL DA_LDAC_B_int                  : std_ulogic;
+   SIGNAL DA_CLR_B_int                   : std_ulogic;
+   SIGNAL QSync                          : std_ulogic_vector(N_QCLICTRL-1 DOWNTO 0);
+    
   COMPONENT dacs_v2
     GENERIC (
       DACS_BUILD_NUMBER : std_logic_vector(15 DOWNTO 0) := X"0010";
@@ -178,7 +185,8 @@ ARCHITECTURE beh OF PDACS_Carbon IS
       DIGIO_BASE_ADDRESS : std_logic_vector (15 DOWNTO 0) := X"0800";
       DIGIO_N_CONNECTORS : integer range 4 DOWNTO 1 := 2;
       DIGIO_FORCE_DIR : std_ulogic_vector := "000000000000";
-      DIGIO_FORCE_DIR_VAL : std_ulogic_vector := "000000000000"
+      DIGIO_FORCE_DIR_VAL : std_ulogic_vector := "000000000000";
+      N_QCLICTRL : integer range 5 downto 0 := 1
     );
     Port (
       fpga_0_rst_1_sys_rst_pin : IN std_logic;
@@ -232,7 +240,11 @@ ARCHITECTURE beh OF PDACS_Carbon IS
       DA_CS_B     : OUT std_logic_vector(N_AO_CHIPS-1 DOWNTO 0);
       DA_LDAC_B   : OUT std_logic;
       DA_SCK      : OUT std_logic;
-      DA_SDI      : OUT std_logic
+      DA_SDI      : OUT std_logic;
+
+      QSync       : OUT    std_ulogic_vector(N_QCLICTRL-1 DOWNTO 0);
+      QSClk       : INOUT  std_logic_vector(N_QCLICTRL-1 DOWNTO 0);
+      QSData      : INOUT  std_logic_vector(N_QCLICTRL-1 DOWNTO 0)
     );
   END COMPONENT;
    
@@ -268,7 +280,8 @@ BEGIN
       IDX_N_CHANNELS => IDX_N_CHANNELS,
       DIGIO_N_CONNECTORS => DIGIO_N_CONNECTORS,
       DIGIO_FORCE_DIR => DIGIO_FORCE_DIR,
-      DIGIO_FORCE_DIR_VAL => DIGIO_FORCE_DIR_VAL
+      DIGIO_FORCE_DIR_VAL => DIGIO_FORCE_DIR_VAL,
+      N_QCLICTRL => N_QCLICTRL
     )
     PORT MAP (
        fpga_0_rst_1_sys_rst_pin       => FPGA_CPU_RESET,
@@ -285,9 +298,15 @@ BEGIN
        fpga_0_RS232_TX_pin            => USB_1_TX,
        
        PTRH_SDA_pin(0)                => IIC_SDA,
-       PTRH_SDA_pin(1)                => BIO(4),
+       PTRH_SDA_pin(1)                => BIO(1),
+       PTRH_SDA_pin(2)                => BIO(3),
+       PTRH_SDA_pin(3)                => BIO(5),
+       PTRH_SDA_pin(4)                => BIO(7),
        PTRH_SCK_pin(0)                => IIC_SCL,
-       PTRH_SCK_pin(1)                => BIO(5),
+       PTRH_SCK_pin(1)                => BIO(0),
+       PTRH_SCK_pin(2)                => BIO(2),
+       PTRH_SCK_pin(3)                => BIO(4),
+       PTRH_SCK_pin(4)                => BIO(6),
        
        subbus_cmdenbl                 => subbus_cmdenbl,
        subbus_cmdstrb                 => subbus_cmdstrb,
@@ -305,22 +324,20 @@ BEGIN
        idx_LimO                       => idx_LimO,
        idx_ZR                         => idx_ZR,
        dig_Dir                        => dig_Dir,
-       dig_IO(63 DOWNTO 0)            => cmd_dio,
-       dig_IO(95 DOWNTO 64)           => cmd_out,
-       dig_IO(111 DOWNTO 96)          => DIO(23 DOWNTO 8),
-       dig_IO(127 DOWNTO 112)         => DIO(63 DOWNTO 48),
-       dig_IO(151 DOWNTO 128)         => DIO(111 DOWNTO 88),
-       dig_IO(158 DOWNTO 152)         => dig_io_nc(18 DOWNTO 12),
-       dig_IO(159)                    => DIO(7),
-       dig_IO(165 DOWNTO 160)         => DIO(29 DOWNTO 24),
-       dig_IO(171 DOWNTO 166)         => dig_io_nc(24 DOWNTO 19),
-       dig_IO(175 DOWNTO 172)         => DIO(39 DOWNTO 36),
-       dig_IO(179 DOWNTO 176)         => dig_io_nc(28 DOWNTO 25),
-       dig_IO(183 DOWNTO 180)         => DIO(47 DOWNTO 44),
-       dig_IO(188 DOWNTO 184)         => dig_io_nc(33 DOWNTO 29),
-       dig_IO(189)                    => DIO(117),
-       dig_IO(190)                    => dig_io_nc(34),
-       dig_IO(191)                    => DIO(119),
+       dig_IO(99 DOWNTO 0)            => cmd_dio,
+       dig_IO(100)                    => DIO(39),
+       dig_IO(101)                    => DIO(49),
+       dig_IO(102)                    => DIO(117),
+       dig_IO(103)                    => DIO(119),
+       dig_IO(153 DOWNTO 104)         => cmd_out,
+       dig_IO(159 DOWNTO 154)         => dig_io_nc(5 DOWNTO 0),
+       dig_IO(183 DOWNTO 160)         => DIO(111 DOWNTO 88),
+       dig_IO(185 DOWNTO 184)         => DIO(17 DOWNTO 16),
+       dig_IO(189 DOWNTO 186)         => DIO(23 DOWNTO 20),
+       dig_IO(191 DOWNTO 190)         => dig_io_nc(7 DOWNTO 6),
+       dig_IO(199 DOWNTO 192)         => DIO(63 DOWNTO 56),
+       dig_IO(200)                    => DIO(7),
+       dig_IO(239 DOWNTO 201)         => dig_io_nc(46 DOWNTO 8),
        ana_in_SDI                     => AI_AD_MISO,
        ana_in_CS5                     => ana_in_CS5,
        ana_in_Conv                    => ana_in_Conv,
@@ -328,16 +345,17 @@ BEGIN
        ana_in_SCK16                   => AI_AD_SCK,
        ana_in_SCK5                    => AI_AFE_SCK,
        ana_in_SDO                     => AI_AFE_MOSI,
-       ctr_PMT(0)                     => COUNT(1),
-       ctr_PMT(1)                     => COUNT(3),
-       ctr_PMT(4 DOWNTO 2)            => COUNT(7 DOWNTO 5),
-       ctr_PMT(10 DOWNTO 5)           => DIO(35 DOWNTO 30),
-       ctr_PMT(11)                    => '0',
-       DA_CLR_B                       => DA_CLR_B,
-       DA_CS_B                        => DA_CS_B,
-       DA_LDAC_B                      => DA_LDAC_B,
-       DA_SCK                         => DA_SCK,
-       DA_SDI                         => DA_SDI
+       ctr_PMT                        => COUNT(-1 DOWNTO 0),
+       DA_CLR_B                       => DA_CLR_B_int,
+       DA_CS_B(1 DOWNTO 0)            => DA_CS_B,
+       DA_CS_B(2)                     => DIO(42),
+       DA_CS_B(3)                     => DIO(43),
+       DA_LDAC_B                      => DA_LDAC_B_int,
+       DA_SCK                         => DA_SCK_int,
+       DA_SDI                         => DA_SDI_int,
+       QSData                         => DIO(3 DOWNTO 1),
+       QSClk                          => DIO(6 DOWNTO 4),
+       QSync                          => QSync
     );
 
     cmd_proc_i : cmd_proc
@@ -359,39 +377,64 @@ BEGIN
   AI_MUX0_A <= ana_in_Row(2 DOWNTO 0);
   AI_MUX1_A <= ana_in_Row(2 DOWNTO 0);
 
-  BIO(3 DOWNTO 0) <= (others => 'Z');
-  BIO(15 DOWNTO 6) <= (others => 'Z');
-  DIO(0) <= not cmd_out(24);
-  DIO(3 DOWNTO 1) <= cmd_out(27 DOWNTO 25);
-  DIO(4) <=	idx_Step(0);
-  DIO(5) <=	idx_Run(0);
-  DIO(6) <=	idx_Dir(0);
-  idx_KillA(0) <= DIO(40);
-  idx_KillB(0) <= DIO(41);
-  idx_LimI(0) <= DIO(42);
-  idx_LimO(0) <= DIO(43);
+  BIO(15 DOWNTO 8) <= (others => 'Z');
+
+  -- DIO(6 downto 1) are QCLI BIO
+  -- DIO(7) is spare DigIO
+  DIO(8) <= cmd_out(24);
+  DIO(9) <= not cmd_out(25);
+  DIO(15 DOWNTO 10) <= cmd_out(31 DOWNTO 26);
+  -- DIO(19,18) are indexer limits
+  -- DIO(23 downto 20) are digIO status
+  DIO(24) <= not cmd_out(32);
+  DIO(27 DOWNTO 25) <= std_logic_vector(QSync);
+  DIO(35 DOWNTO 28) <= cmd_out(40 DOWNTO 33);
+  DIO(36) <=	idx_Step(0);
+  DIO(37) <=	idx_Dir(0);
+  DIO(38) <=	idx_Run(0);
+  -- DIO(39) is dig_IO(100)
+  DA_SCK <= DA_SCK_int;
+  DIO(40) <= DA_SCK_int;
+  DA_SDI <= DA_SDI_int;
+  DIO(41) <= DA_SDI_int;
+  -- DIO(43, 42) are DA_CS_B(3,2)
+  DA_LDAC_B <= DA_LDAC_B_int;
+  DIO(44) <= DA_LDAC_B_int;
+  DA_CLR_B <= DA_CLR_B_int;
+  DIO(45) <= DA_CLR_B_int;
+  DIO(47 DOWNTO 46) <= cmd_out(49 DOWNTO 48);
+  DIO(48) <= not cmd_out(41);
+  -- DIO(49) is dig_IO(101)
+  DIO(55 DOWNTO 50) <= cmd_out(47 DOWNTO 42);
+  -- DIO(63 DOWNTO 56) is dig_IO(199 DOWNTO 192)
   DIO(87 DOWNTO 64) <= cmd_out(23 DOWNTO 0);
+  -- DIO(111 DOWNTO 88) is dig_IO(183 DOWNTO 160)  
   DIO(112) <= ana_in_row(3);
   DIO(113) <= subbus_fail_leds(0);
   DIO(114) <= ana_in_row(4);
   DIO(115) <= subbus_fail_leds(1);
   DIO(116) <= ana_in_row(5);
+  -- DIO(117) is dig_IO(176)
   DIO(118) <= subbus_flt_cpu_reset;
-
-  DIO_DIR(0) <= dig_dir(19);
-  DIO_DIR(1) <= dig_dir(12);
-  DIO_DIR(2) <= dig_dir(13);
-  DIO_DIR(3) <= dig_dir(20);
-  DIO_DIR(4) <= dig_dir(21);
-  DIO_DIR(5) <= dig_dir(22);
-  DIO_DIR(6) <= dig_dir(14);
-  DIO_DIR(7) <= dig_dir(15);
-  DIO_DIR(10 DOWNTO 8) <= "000"; -- Power board outputs
-  DIO_DIR(11) <= dig_dir(16);
-  DIO_DIR(12) <= dig_dir(17);
-  DIO_DIR(13) <= dig_dir(18);
-  DIO_DIR(14) <= dig_dir(23);
+  -- DIO(119) is dig_IO(177)
   
+  idx_KillA(0) <= '0';
+  idx_KillB(0) <= '0';
+  idx_LimI(0) <= DIO(18);
+  idx_LimO(0) <= DIO(19);
+
+  DIO_DIR(0) <= '0'; -- goes nowhere
+  DIO_DIR(1) <= '0';
+  DIO_DIR(2) <= dig_dir(23);
+  DIO_DIR(6 DOWNTO 3) <= "0000";
+  DIO_DIR(7) <= dig_dir(24);
+  DIO_DIR(10 DOWNTO 8) <= "000"; -- Power board outputs
+  DIO_DIR(11) <= dig_dir(20);
+  DIO_DIR(12) <= dig_dir(21);
+  DIO_DIR(13) <= dig_dir(22);
+  DIO_DIR(14) <= '0';
+
+
   DIO_OE <= '1';
   DIO_OE_B <= '0';
   DACS_switches(7 DOWNTO 4) <= GPIO_SW;
