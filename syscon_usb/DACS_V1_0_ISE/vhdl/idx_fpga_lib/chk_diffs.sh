d31eec69ba7dec265af240fe07279b783c007f56
Norton Allen <allen@huarp.harvard.edu>
2012-01-12 17:35:26 +0000

    Check script and updates for type consistency

---
+++ syscon_usb/DACS_V1_0_ISE/vhdl/idx_fpga_lib/chk_diffs.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+H=/cygdrive/c/HDS/idx_fpga/idx_fpga_lib/hdl
+
+for i in *.vhd*; do
+  echo
+  echo $i
+  if [ -f $H/$i ]; then
+    diff -b $i $H/$i
+  else
+    echo "  $H/$i not found"
+  fi
+done | less
