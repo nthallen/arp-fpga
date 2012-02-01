a03de9ce0529b6a78863389cc36144484934134b
Norton Allen <allen@huarp.harvard.edu>
2012-02-01 18:20:23 +0000

    Build 24 with probe on WrEnB routed to J172.6

--- syscon_usb/DACS_V1_0_ISE/vhdl/idx_fpga_lib/dacs_v2.vhdl
+++ syscon_usb/DACS_V1_0_ISE/vhdl/idx_fpga_lib/dacs_v2.vhdl
@@ -103,7 +103,8 @@ entity dacs_v2 is
       QSync       : OUT    std_ulogic_vector(N_QCLICTRL-1 DOWNTO 0);
       QSClk       : INOUT  std_logic_vector(N_QCLICTRL-1 DOWNTO 0);
       QSData      : INOUT  std_logic_vector(N_QCLICTRL-1 DOWNTO 0);
-      QNBsy       : IN     std_logic_vector(N_QCLICTRL-1 DOWNTO 0)
+      QNBsy       : IN     std_logic_vector(N_QCLICTRL-1 DOWNTO 0);
+      Probe       : OUT    std_logic_vector(N_QCLICTRL-1 DOWNTO 0)
     );
 end dacs_v2;
 
@@ -345,7 +346,8 @@ architecture Behavioral of dacs_v2 is
         QSync  : OUT    std_ulogic;
         RData  : OUT    std_logic_vector(15 DOWNTO 0);
         QSClk  : INOUT  std_logic;
-        QSData : INOUT  std_logic
+        QSData : INOUT  std_logic;
+        Probe  : OUT    std_logic
      );
   END COMPONENT;
   
@@ -612,7 +614,8 @@ begin
          RData  => iRData(16*(QCLICTRL0+i)+15 DOWNTO 16*(QCLICTRL0+i)),
          QSClk  => QSClk(i),
          QSData => QSData(i),
-         QNBsy  => QNBsy(i)
+         QNBsy  => QNBsy(i),
+         Probe  => Probe(i)
       );
   end generate;
 
