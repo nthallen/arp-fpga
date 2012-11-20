5464c96ea10cbb69cd434ba49afc15c3f525cb70
Norton Allen <allen@huarp.harvard.edu>
2012-11-20 19:19:55 +0000

    Added faster response to purge request button.

--- syscon_usb/idx_fpga/idx_fpga_lib/hdl/lk204_tester_sim.vhdl
+++ syscon_usb/idx_fpga/idx_fpga_lib/hdl/lk204_tester_sim.vhdl
@@ -14,18 +14,21 @@ USE ieee.std_logic_arith.all;
 
 ENTITY lk204_tester IS
    PORT( 
-      BdIntr : IN     std_ulogic;
-      ExpAck : IN     std_ulogic;
-      RData  : IN     std_logic_vector (15 DOWNTO 0);
-      Addr   : OUT    std_logic_vector (15 DOWNTO 0);
-      ExpRd  : OUT    std_ulogic;
-      ExpWr  : OUT    std_ulogic;
-      F8M    : OUT    std_ulogic;
-      INTA   : OUT    std_ulogic;
-      Rst    : OUT    std_logic;
-      WData  : OUT    std_logic_vector (15 DOWNTO 0);
-      scl    : INOUT  std_logic_vector (0 TO 0);
-      sda    : INOUT  std_logic_vector (0 TO 0)
+      BdIntr   : IN     std_ulogic;
+      ExpAck   : IN     std_ulogic;
+      RData    : IN     std_logic_vector (15 DOWNTO 0);
+      Addr     : OUT    std_logic_vector (15 DOWNTO 0);
+      ExpRd    : OUT    std_ulogic;
+      ExpWr    : OUT    std_ulogic;
+      F8M      : OUT    std_ulogic;
+      INTA     : OUT    std_ulogic;
+      RE       : OUT    std_logic;
+      Rst      : OUT    std_logic;
+      WData    : OUT    std_logic_vector (15 DOWNTO 0);
+      switches : OUT    std_logic_vector (7 DOWNTO 0);
+      scl      : INOUT  std_logic_vector (0 TO 0);
+      sda      : INOUT  std_logic_vector (0 TO 0);
+      LEDs     : IN     std_ulogic_vector (7 DOWNTO 0)
    );
 
 -- Declarations
@@ -36,6 +39,7 @@ END lk204_tester ;
 ARCHITECTURE sim OF lk204_tester IS
    SIGNAL Done   : std_logic;
    SIGNAL Read_Result : std_logic_vector(15 DOWNTO 0); 
+   SIGNAL switches_int: std_logic_vector(1 DOWNTO 0);
 BEGIN
   clock : Process
   Begin
@@ -104,6 +108,7 @@ BEGIN
     INTA <= '0';
     scl(0) <= 'H';
     sda(0) <= 'H';
+    switches_int <= "00";
     -- pragma synthesis_off
     wait until F8M'Event AND F8M = '1';
     wait until F8M'Event AND F8M = '1';
@@ -163,6 +168,8 @@ BEGIN
     
     sbwr( X"1102", X"01FE" );
     sbrd( X"1104" );
+    assert(Read_Result(15 downto 8) = X"00")
+      report "Expected no switches or LEDs asserted" severity error;
     wait for 350 us;
     sbwr( X"1102", X"01A0" );
     sbrd( X"1104" );
@@ -172,9 +179,53 @@ BEGIN
     wait for 10 ms;
     sbrd( X"1104" );
     wait for 350 us;
+    switches_int <= "01"; -- Assert Purge switch
+    wait for 150 ms;
+    assert(LEDs(4) = '0')
+      report "Expected Purge LED" severity error;
+    wait for 150 ms;
+    sbrd( X"1104" );
+    assert(Read_Result(15 downto 8) = X"11")
+      report "Expected Purge switch and LED status" severity error;
+    switches_int <= "00"; -- Release Purge switch;
+    wait for 150 ms;
+    assert(LEDs(4) = '0')
+      report "Expected Purge LED to stay on after switch release"
+      severity error;
+    sbrd(X"1104");
+    assert(Read_Result(15 downto 8) = X"10")
+      report "Status invalid: expected Purge LED w/o switch"
+      severity error;
+    sbwr(X"1104", X"0010"); -- Clear Purge LED
+    wait for 10 ms;
+    assert(LEDs(4) = '1')
+      report "Purge LED should be off after write"
+      severity error;
+    sbwr(X"1104", X"00A0"); -- Set Land LED
+    wait for 10 ms;
+    assert(LEDs(5) = '0')
+      report "Land LED should be on after set command"
+      severity error;
+    sbwr(X"1104", X"0090"); -- Set Purge LED
+    wait for 10 ms;
+    assert(LEDs(4) = '0')
+      report "Purge LED should be on after set command"
+      severity error;
+    sbwr(X"1104", X"0030"); -- Clear both LEDs
+    wait for 10 ms;
+    assert(LEDs(5 downto 4) = "11")
+      report "Both LEDs should be off after clear command"
+      severity error;
+    wait for 10 ms;
     Done <= '1';
     wait;
     -- pragma synthesis_on
   End Process;
+  
+  switches(7 downto 6) <= "00";
+  switches(5 downto 4) <= not std_logic_vector(LEDs(5 downto 4));
+  switches(3 downto 2) <= "00";
+  switches(1 downto 0) <= switches_int;
+  RE <= '1';
 END ARCHITECTURE sim;
 
