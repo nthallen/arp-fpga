0c3ec20fe30048dca0141cc81d726f34624b5a8d
Norton Allen <allen@huarp.harvard.edu>
2019-07-22 19:41:11 -0400

    Earlier updates after upgrade to VirtualBox ISE

--- syscon_usb/DACS_V1_0_ISE/Processor.synth.tcl
+++ syscon_usb/DACS_V1_0_ISE/Processor.synth.tcl
@@ -1,5 +1,5 @@
 proc pnsynth {} {
-  cd C:/Users/nort.ARP/Documents/SW/arp-fpga/syscon_usb/DACS_V1_0_ISE/Processor
+  cd /home/ise/Exp/arp-fpga/syscon_usb/DACS_V1_0_ISE/Processor
   if { [ catch { xload xmp Processor.xmp } result ] } {
     exit 10
   }
